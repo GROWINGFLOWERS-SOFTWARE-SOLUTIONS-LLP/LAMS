91479c5a3040dec319b9648ae86b0e07d6d50adb
sahil-mhatre-18 <sahil.mhatre@growingflowers-solutions.com>
2024-08-30 10:48:36 +0530

    Attendance Page UI Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AttendanceComponent } from './Pages/Components/attendance/attendance.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AttendanceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
