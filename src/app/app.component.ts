a6b265db46b011a3eee65c39d1ffbcdf1a47e008
Abhishek-bagad <abhishek.bagad@growingflowers-solution.com>
2024-08-30 11:22:37 +0530

    History page UI completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HistoryComponent } from './Pages/Components/history/history.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HistoryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
