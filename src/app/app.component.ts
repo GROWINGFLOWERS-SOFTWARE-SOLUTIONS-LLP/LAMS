56e7c20d4a20e7c58afde0f8178d96f53a0c8156
Prajakta-Badhan <prajakta.badhan@growingflowers-solutions.com>
2024-08-30 10:16:45 +0530

    Holiday Page UI Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LeaveComponent } from './Pages/Components/leave/leave.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LeaveComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
