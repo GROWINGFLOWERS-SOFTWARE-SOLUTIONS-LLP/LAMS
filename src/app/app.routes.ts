e4c042f1e55824b41d8dd065c7fa5fc0cb1423ac
aditya-bhise <aditya.bhise@growingflowers-solutions.com>
2024-09-05 11:46:33 +0530

    login page updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import { Routes} from '@angular/router';
 import { LeaveComponent } from './Pages/Components/leave/leave.component';
 import { HolidaysComponent } from './Pages/Components/holidays/holidays.component';
 import { NavbarComponent } from './Pages/Layout/navbar/navbar.component';
@@ -6,17 +6,18 @@ import { HistoryComponent } from './Pages/Components/history/history.component';
 import { HelpComponent } from './Pages/Components/help/help.component';
 import { AttendanceComponent } from './Pages/Components/attendance/attendance.component';
 import { DashboardComponent } from './Pages/Components/dashboard/dashboard.component';
+import { LoginComponent } from './Pages/Auth/login/login.component';
 
 export const routes: Routes = [
-    // {
-    //     path:'',
-    //     redirectTo:'login',
-    //     pathMatch:'full'
-    //   },
-    //   {
-    //     path:'login',
-    //     component:LoginComponent
-    //   },
+    {
+        path:'',
+        redirectTo:'login',
+        pathMatch:'full'
+      },
+      {
+        path:'login',
+        component:LoginComponent,
+      },
     
     {
         path: '',
