df58f984e2fa96be04630049e74bc75a472534b5
aditya-bhise <aditya.bhise@growingflowers-solutions.com>
2024-11-06 09:25:15 +0530

    changes have been made

--- src/app/Pages/Auth/login/login.component.ts
+++ src/app/Pages/Auth/login/login.component.ts
@@ -64,7 +64,7 @@ export class LoginComponent implements OnInit {
   }
  
   loginFun() {
-    this.loading = true;
+    this.loading = false;
     this.cdr.detectChanges();
  
     // Simulate 2-second delay for loader (whether login is successful or not)
