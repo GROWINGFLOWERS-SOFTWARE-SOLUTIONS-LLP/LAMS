33969779666504f7e86c20d29865b68d76e8b51d
Prajakta-Badhan <prajakta.badhan@growingflowers-solutions.com>
2024-11-12 12:13:21 +0530

    updated

--- src/app/Core/Services/api.service.ts
+++ src/app/Core/Services/api.service.ts
@@ -187,8 +187,6 @@ export class ApiService {
     return this.http.put(`${this.apiUrl}/employees/${updatedUser.id}`, updatedUser);
   }
 
-
-
   
   getProjects(): Observable<any[]> {
     return this.http.get<any[]>(`${this.apiUrl}/projects`);
